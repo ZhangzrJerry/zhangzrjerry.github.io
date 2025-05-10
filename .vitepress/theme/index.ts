15244225bebc6b600abce1a41e3d2fab8d217e55
ZhangzrJerry <ZhangzrJerry@outlook.com>
2025-05-11 01:39:31 +0800

    在主题布局中根据环境变量调整Umami分析脚本的引入逻辑，开发环境下不再加载该脚本，以优化开发体验。

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -12,18 +12,18 @@ import CustomHomeLayout from './components/CustomHome.vue'
 import VisitorMap from './components/VisitorMap.vue'
 import BetterTable from './components/BetterTable.vue'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      'layout-bottom': () => [
-        h(VisitorMap),
-        h('script', {
+      'layout-bottom': () =>
+        isDev ? null : [h('script', {
           defer: true,
           src: 'https://cloud.umami.is/script.js',
           'data-website-id': '61765fc4-2042-401e-801b-d8032e948a83'
-        })
-      ],
+        }), VisitorMap]
     })
   },
   enhanceApp({ app, router, siteData }) {
